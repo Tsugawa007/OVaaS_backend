932ea67012a08f53b46f66372ed5c3de0b3dc9be
Maiko007 <59519629+Maiko007@users.noreply.github.com>
2020-11-20 02:46:29 +0900

    Update __init__.py

--- handwritten/__init__.py
+++ handwritten/__init__.py
@@ -68,6 +68,7 @@ def main(req: func.HttpRequest,context: func.Context) -> func.HttpResponse:
             channel = grpc.insecure_channel("{}:{}".format(_HOST, _PORT))
             stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
             output = stub.Predict(request,timeout = 10.0)
+            logging.warning(f'Output:{output}')
             logging.warning(f'Grpc Success')
 
 

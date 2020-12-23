c71ce6fb032152f2d4ab23dd38f220d59e062cf1
Maiko007 <59519629+Maiko007@users.noreply.github.com>
2020-12-23 23:19:49 +0900

    Update __init__.py

--- colorization/__init__.py
+++ colorization/__init__.py
@@ -9,8 +9,6 @@ import cv2
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     _HOST = os.environ.get("VM_IPADDRESS")
     _PORT = os.environ.get("COLORIZATION_PORT")
-    #_HOST = prep.__get_config__('COLORIZATION', 'host')
-    #_PORT = prep.__get_config__('COLORIZATION', 'port')
     _NAME = prep.__get_config__('COLORIZATION', 'name')
     _MODEL_NAME = prep.__get_config__('COLORIZATION', 'model_name')
 
@@ -19,7 +17,7 @@ def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
 
     method = req.method
     url = req.url
-    # files = req.files[_NAME]
+
     files = req.files['image']
     logging.info(f"files is {files}")
     if method != 'POST':
@@ -34,13 +32,12 @@ def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     # pre processing
     input_image = prep.create_input_image(files)  # get image form request
     logging.info(f'Input_Image Success.')
-    # original_frame = cv2.imread(input_image)
+ 
     try:
         img_bgr_out = prep.RemoteColorization(_HOST, _PORT, _MODEL_NAME).infer(input_image) 
-        #colorization = prep.RemoteColorization(_HOST, _PORT, _MODEL_NAME)
        
         logging.info(f"outpre success!")
-        #img_bgr_out=colorization.infer(input_image)
+        
     except Exception as e:
         if 'StatusCode.DEADLINE_EXCEEDED' in str(e):
             logging.error(e)
@@ -56,12 +53,11 @@ def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     logging.info(f"Inference complete,Takes{time_cost}")
 
     logging.info(f"Successfully.img_bgr_out is {img_bgr_out}.\nCreate output image.")
-    #final_image = prep.create_output_image(img_bgr_out, img_bgr_out)
     final_image = prep.create_output_image(input_image, img_bgr_out)
     logging.info(f"Successfully.final_image is {final_image}.")
     mimetype = 'image/jpeg'
     
-    # img_output_bytes = Image.fromarray(final_image)
+  
     img_output_bytes = prep.cv2ImgToBytes(final_image)
     logging.info(f"Successfully.img_output_bytes is {img_output_bytes}.")
     return func.HttpResponse(body=img_output_bytes, status_code=200, mimetype=mimetype, charset='utf-8')
